d44080119c679003acc45bd74bf62e6c5e2c901a
gabrielhassan95 <ghsammour01@gmai.com>
2024-07-27 07:07:39 -0300

    estudandos herança, polimorfismo, interface.

---
+++ exemplo_pacotes/src/Autodromo.java
@@ -0,0 +1,12 @@
+
+public class Autodromo {
+
+	public static void main(String[] args) {
+		Carro jeep = new Carro();
+		jeep.setChassi("222412");
+		jeep.ligar();
+
+	Moto z400 = new Moto();
+		z400.setChassi("42161");
+	}
+}
---
+++ exemplo_pacotes/src/Carro.java
@@ -0,0 +1,16 @@
+
+public class Carro extends Veiculo {
+	
+	public void ligar() {
+		confereCambio();
+		confereCombustivel();
+		System.out.println("Carro Ligado");
+	}
+	private void confereCombustivel() {
+		System.out.println("Conferindo combustivel");
+		}
+	private void confereCambio() {
+		System.out.println("conferindo cambio em P");
+	}
+}
+
